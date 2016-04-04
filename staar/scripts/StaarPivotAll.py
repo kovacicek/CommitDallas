8f0cd06f8c583db61ffd08c4030db3e774c905c7
Milan Kovacic <kovacicek@hotmail.com>
2016-04-05 00:11:25 +0200

    iterating through value columns tuple

--- staar/scripts/StaarPivotAll.py
+++ staar/scripts/StaarPivotAll.py
@@ -13,47 +13,35 @@ from pandas import ExcelWriter, read_csv, concat, merge, pivot, pivot_table
 from pandas.core.frame import DataFrame
 
 # Columns related to pivoting
-pivot_col = "Category"
-value_col = "econ"
+
 
 # Columns that will be extracted from the files
-ColumnsCampus = ["CAMPUS",
-                 "YEAR",
-                 "REGION",
-                 "DISTRICT",
-                 "DNAME",
-                 "CNAME",
-                 "Subject",
-                 "Grade",
-                 "Language",
-                 pivot_col,
-                 value_col
-                 ]
-
-ColumnsDS = ["YEAR",
-             "REGION",
-             "DISTRICT",
-             "DNAME",
-             "Subject",
-             "Grade",
-             "Language",
-             pivot_col,
-             value_col
-             ]
-
+def cols_generator():
+    pivot = 'Category'
+    values = ('all', 'atry', 'econ', 'ecoy', 'etha', 'ethb', 'ethh', 'ethi',
+              'ethp', 'ethv', 'ethw', 'eth2', 'lepc', 'sexf', 'sexm', 'sexv')
+    base_cols_district = ["YEAR", "REGION", "DISTRICT", "DNAME", "Subject",
+                          "Grade", "Language"]
+    base_cols_campus = ["CAMPUS", "YEAR", "REGION", "DISTRICT", "DNAME",
+                        "CNAME", "Subject", "Grade", "Language"]
+    for value in values:
+        cols_district = base_cols_district + [pivot, value]
+        cols_campus = base_cols_campus + [pivot, value]
+        yield (cols_campus, cols_district, pivot, value)
+        
 
 class StaarPivotAll:
-    script_name = "StaarPivotAll"
-
     def __init__(self, input_dir, output_dir):
         self.input_dir = input_dir
         self.output_dir = output_dir
 
-        self.Process()
+        for cols in cols_generator():
+            self.Process(*cols)
     # end __init__
 
-    def Process(self):
-        print("Processing started")
+    def Process(self, cols_campus, cols_district, pivot_col, value_col):
+        print("Processing started\nPivot column: %s\nValue column: %s" 
+              % (pivot_col, value_col))
         # List directory containing the .csv files
         for filename in listdir(self.input_dir):
             fn = path.splitext(filename)[0]
@@ -62,37 +50,38 @@ class StaarPivotAll:
                 try:
                     if "campus" in fn:
                         df = read_csv(file_path,
-                                      usecols=ColumnsCampus,
+                                      usecols=cols_campus,
                                       delimiter=",",
                                       header=0,
                                       low_memory=False)
                         df_pivot = df.set_index(
-                            ColumnsCampus[:-1]).unstack(pivot_col)
+                            cols_campus[:-1]).unstack(pivot_col)
                     elif 'district-state' in fn:
                         df = read_csv(file_path,
-                                      usecols=ColumnsDS,
+                                      usecols=cols_district,
                                       delimiter=",",
                                       header=0,
                                       low_memory=False)
                         df_pivot = df.set_index(
-                            ColumnsDS[:-1]).unstack(pivot_col)
+                            cols_district[:-1]).unstack(pivot_col)
                     else:
                         print("\t Skipping file: %s" % file_path)
                         
-                    self.WriteData(df_pivot, filename)
+                    self.WriteData(df_pivot, filename, value_col)
                 except OSError:
                    print("Error while reading %s" % filename)
     # end ReadData
 
     def WriteData(self,
                   data_frame,
-                  output_name):
+                  output_name,
+                  value):
         """
         Write data_frame
         """
         if not exists(self.output_dir):
             mkdir(self.output_dir)
-        output_name = output_name.replace("merged", "pivoted_%s" % value_col)
+        output_name = output_name.replace("merged", "pivoted_%s" % value)
         print("\n\tWriting %s" % output_name)
         # fix the column names after multiindexing
         data_frame.reset_index(col_level=1, inplace=True)
@@ -101,9 +90,9 @@ class StaarPivotAll:
         # add demo column
         print("\tAdding demo column in: %s" % output_name)
         if "campus" in output_name:
-            data_frame.insert(9, 'demo', value_col)
+            data_frame.insert(9, 'demo', value)
         elif 'district-state' in output_name:
-            data_frame.insert(7, 'demo', value_col)
+            data_frame.insert(7, 'demo', value)
         else:
             print("\tDemo column not added in: %s" % output_name)
         
